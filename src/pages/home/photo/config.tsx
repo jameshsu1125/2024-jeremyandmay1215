9d523c371ba217c3496c09b5fb4d7e9a005d5a6b
jameshsu1125 <jameshsu1125@gmail.com>
2024-10-29 18:14:29 +0800

    Update config.tsx

--- src/pages/home/photo/config.tsx
+++ src/pages/home/photo/config.tsx
@@ -1,7 +1,6 @@
-import photo1 from './img/00221-2314158840.jpeg';
-import photo2 from './img/571855.png';
-import photo3 from './img/cm08gr4yu06cpnkdmz0esqb1c_original.jpeg';
-import photo4 from './img/cm0frqep303uif0qalholp66s_original.jpeg';
-import photo5 from './img/cm0fs3bm303i4ktgx4efyfj27_original.jpeg';
+import photo1 from './img/0.jpeg';
+import photo3 from './img/2.jpeg';
+import photo4 from './img/3.jpeg';
+import photo5 from './img/4.jpeg';
 
-export const photos = [photo1, photo2, photo3, photo4, photo5];
+export const photos = [photo1, photo3, photo4, photo5];
